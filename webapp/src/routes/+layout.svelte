635068edd94d3cd5b899198e959183ee970564db
Puria Nafisi Azizi <puria@dyne.org>
2025-02-19 20:39:57 +0100

    feat: add chat support (#984)

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -17,4 +17,20 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <ParaglideJS {i18n}>
 	<slot />
+	<script>
+  (function(d,t) {
+    var BASE_URL="https://chatwoot.ss.did.co.im";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.defer = true;
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+      window.chatwootSDK.run({
+        websiteToken: 'Rr6aY328gJ5TnF9Cu2uvucQQ',
+        baseUrl: BASE_URL
+      })
+    }
+  })(document,"script");
+  </script>
 </ParaglideJS>
