e2366831c9e3693741dc2ab53f71cad75e7f03e8
matteo-cristino <cristino.matteo97@gmail.com>
2025-09-30 18:56:15 +0200

    fix: parse verification template claims field instead of schema

--- webapp/src/lib/components/templateSchemaDisplay.svelte
+++ webapp/src/lib/components/templateSchemaDisplay.svelte
@@ -10,20 +10,31 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	import type { TemplatesResponse } from '$lib/pocketbase/types';
 	import {
 		objectSchemaToClaims,
-		flattenClaimsProperties
+		flattenClaimsProperties,
+		type ClaimsProperty
 	} from '@api/download-microservices/utils/credential-subject';
 	import { DEFAULT_LOCALE } from '@api/download-microservices/utils/locale';
-	import { Effect, pipe, Either } from 'effect';
+	import { Effect, pipe, Either, Array as A } from 'effect';
 	import EmptyState from './emptyState.svelte';
 	import { ExclamationTriangle } from 'svelte-heros-v2';
+
+	//
+
+	type Claim = {
+		id: string
+		name: string
+		selected: boolean
+		type: string
+		values: string
+	}
+
+	type ClaimsPropertyValues = ClaimsProperty & { values?: string }
 
 	//
 
 	export let template: TemplatesResponse;
 
 	//
-
-	let propertyListEither = getTemplatePropertyList(template);
 
 	function getTemplatePropertyList(template: TemplatesResponse) {
 		return pipe(
@@ -32,22 +43,54 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 					template.schema as ObjectSchema,
 					objectSchemaToClaims,
 					flattenClaimsProperties
-				)
+				) as [string, ClaimsPropertyValues][]
 			),
 			Effect.either,
 			Effect.runSync
 		);
 	}
+	function getTemplatePropertyListFromClaims(template: TemplatesResponse) {
+		return pipe(
+			Effect.try(() =>
+				pipe(
+					template.claims as Claim[],
+					A.filter((claim): claim is Claim => claim !== undefined && claim.selected),
+					A.map((claim) => [
+						claim.id,
+							{
+								display: [{ name: claim.name, locale: 'en-US', id: '' }],
+								mandatory: false,
+								values: claim.values,
+								path: [],
+							}
+						]
+					)
+				) as [string, ClaimsPropertyValues][]
+			),
+			Effect.either,
+			Effect.runSync
+		);
+	}
+
+	// Usage:
+	let propertyListEither;
+	if (template.schema !== "") {
+		propertyListEither = getTemplatePropertyList(template);
+	} else {
+		propertyListEither = getTemplatePropertyListFromClaims(template);
+	}
+
 </script>
 
 <div class="divide-y rounded-lg border bg-gray-50">
-	{#if propertyListEither.pipe(Either.isLeft)}
+	{#if Either.isLeft(propertyListEither)}
 		<EmptyState title={m.Template_parsing_error()} icon={ExclamationTriangle} />
 	{:else}
 		{@const propertyList = propertyListEither.pipe(Either.getOrThrow)}
 
 		{#each propertyList as [propertyId, property]}
 			{@const displayName = property.display?.at(0)?.name}
+			{@const values = property.values}
 			<div class="p-4">
 				<p>
 					{m.Property_ID()}: <span class="font-mono text-primary-700">{propertyId}</span>
@@ -56,6 +99,12 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 					<p>
 						{m.Display_name()}:
 						<span class="text-primary-700">{displayName} ({DEFAULT_LOCALE})</span>
+					</p>
+				{/if}
+				{#if values}
+					<p>
+						{m.Values()}:
+						<span class="text-primary-700">{values}</span>
 					</p>
 				{/if}
 
